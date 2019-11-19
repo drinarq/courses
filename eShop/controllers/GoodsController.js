8a3fd1e0325b34174a9bcf6b7cfc70751ba68f15
drinarq <drinarq@gmail.com>
2019-11-19 11:56:08 +0300

    add Controller for Goods

--- eShop/controllers/GoodsController.js
+++ eShop/controllers/GoodsController.js
@@ -1,7 +1,7 @@
 const express=require('express');
 
 exports.GoodsAdd=function(req,res){
-    res.send(JSON.stringify([req.body.price,req.body.id,req.body.name,req.body.]));
+    res.send(JSON.stringify(req.body));
 };
 
 
