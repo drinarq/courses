816f0c7ebcab2061fd842bea90901a719cb11895
drinar <drinarq@gmail.com>
2020-01-14 13:05:18 +0300

    update index

--- eShop/index.js
+++ eShop/index.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const app = express();
-const GoodsRouter = require("F:\\courses\\eShop\\routes\\GoodsRoouter.js");
-app.use("/goods",GoodsRouter );
+const pasport=require('passport');
+const session=require('express-session');
+const GoodsRouter = require('./routes/goods.js');
+const UserRouter = require('./routes/user.js');
+const MarkRouter=require('./routes/mark.js');
+const LoginRouter=require('./routes/authorization.js');
+const RoleRouter=require('./routes/role.js');
+const passportLocal=require('./passport.js')
+const sessionConfig=require('./configuration/session.json');
+
+app.use(session(sessionConfig));
+passportLocal(pasport);
+app.use(pasport.initialize());
+app.use(pasport.session());
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use("/Goods",GoodsRouter);
+app.use("/User",UserRouter);
+app.use("/authenticate",LoginRouter);
+app.use("/roles",RoleRouter);
+app.use("/mark",MarkRouter);
 app.use(function (req, res, next) {
     res.status(404).send("Not Found")
 });
+
 
 app.listen(3000);
