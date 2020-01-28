b1f641bfeb8f470a42f1b96bb45ab924729faa5f
drinar <drinarq@gmail.com>
2020-01-28 14:34:14 +0300

    add ResponceMessage class

--- eShop/helpers/helper.js
+++ eShop/helpers/helper.js
@@ -1,2 +1,24 @@
-const DPassword="01282000vladdd";
-module.exports=DPassword;
+class ResponceMessage {
+    OK(status = 200, message = '', data = []) {
+        return {
+            status: status,
+            message: message,
+            data: data,
+            success: true
+        }
+    }
+
+    ERROR(error) {
+        return {
+            status: error.status,
+            message: 'Error',
+            error: {
+                name: error.name,
+                message: error.message,
+            },
+            success: false
+        }
+    }
+}
+
+module.exports = new ResponceMessage();
