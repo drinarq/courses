b6bb4030a3259fc3196759e0d9eb5d394fdd6e25
drinar <drinarq@gmail.com>
2020-01-28 14:56:09 +0300

    add new routes

--- eShop/index.js
+++ eShop/index.js
@@ -4,10 +4,9 @@ const pasport=require('passport');
 const session=require('express-session');
 const GoodsRouter = require('./routes/goods.js');
 const UserRouter = require('./routes/user.js');
-const MarkRouter=require('./routes/mark.js');
 const LoginRouter=require('./routes/authorization.js');
 const RoleRouter=require('./routes/role.js');
-const passportLocal=require('./passport.js')
+const passportLocal=require('./passport.js');
 const sessionConfig=require('./configuration/session.json');
 
 app.use(session(sessionConfig));
@@ -20,7 +19,6 @@ app.use("/Goods",GoodsRouter);
 app.use("/User",UserRouter);
 app.use("/authenticate",LoginRouter);
 app.use("/roles",RoleRouter);
-app.use("/mark",MarkRouter);
 app.use(function (req, res, next) {
     res.status(404).send("Not Found")
 });
