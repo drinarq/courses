0bfc6ee157da0145738ada99fce2d516619540d2
drinar <drinarq@gmail.com>
2020-01-28 13:50:36 +0300

    the method of getting the role is changed

---
+++ eShop/middleware/adminCheck.js
@@ -0,0 +1,18 @@
+const adminConst = require('../helpers/constRole.js');
+const NotEnoughRootsExeption = require('../errors/RootsExeption.js');
+const UnauthorizedExeption = require('../errors/UnautorizedExeption.js');
+const RoleService=require('../services/role.js');
+module.exports = async function(req, res, next) {
+    if (req.user) {
+
+        const role = await RoleService.getUserRole(req.user.id);
+            if (role.value === adminConst.admin) {
+                return next();
+            }
+
+
+        next(new NotEnoughRootsExeption('Not enough roots.'));
+    }
+
+    next(new UnauthorizedExeption('Login to get access'));
+};
