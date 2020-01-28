56877c156a33d3651b73516aecd07d8e63d4fb11
drinar <drinarq@gmail.com>
2020-01-28 14:49:36 +0300

    add services

---
+++ eShop/services/role.js
@@ -0,0 +1,40 @@
+const rolesRepository = require('../repository/roles.js');
+
+class RolesService {
+    async getRole(id) {
+        const role = await rolesRepository.getRole(id);
+
+        return role;
+    }
+
+    async getRoleByValue(value) {
+        const role = await rolesRepository.getRoleByValue(value);
+
+        return role;
+    }
+
+    async getAllRoles() {
+        const roles = await rolesRepository.getAllRoles();
+
+        return roles;
+    }
+
+    async addRole(role) {
+        await rolesRepository.addRole(role);
+    }
+
+    async deleteRole(id) {
+        await rolesRepository.deleteRole(id);
+    }
+
+    async updateRole(id, data) {
+        await rolesRepository.updateRole(id, data);
+    }
+
+    async getUserRole(id){
+        const userRole=await rolesRepository.getUserRole(id);
+        return userRole;
+    }
+}
+
+module.exports = new RolesService();
--- eShop/services/user.js
+++ eShop/services/user.js
@@ -1,10 +1,107 @@
 const userRep=require('../repository/user.js');
+const constRole = require('../helpers/constRole.js');
+const roleService = require('./role.js');
+const RootsExeption = require('../errors/RootsExeption.js');
+const NotFound = require('../errors/NotFound.js');
+const AlreadyExist=require('../errors/AlredyExist.js');
+const userRoleRep=require('../repository/roles.js');
+class User{
+    async getUserByEmail(email) {
+        const user = await usersRepository.getUserByEmail(email);
 
-class UserServices{
+        return user;
+    }
+    async addUser(value){
 
-    async addUser(user){
-        await userRep.appUser(user);
+        const UserExist=!!(await userRep.getUserByEmail(value.email));
+        if(UserExist){
+            throw new AlreadyExist(`user with ${value.email} already exist`);
+        }
+        const user= await userRep.addUser(value);
+
+        const role=await roleService.getRoleByValue(constRole.user);
+
+        if(!role){
+            throw new NotFound(`role ${role} not found`)
+        }
+
+        await userRoleRep.addUserRole(user.id,role.id);
+
+
+    }
+
+    async getUsers(){
+        const users=await userRep.getUsers();
+
+        return users;
+    }
+
+    async deleteUser(id){
+
+        const user=  await userRep.getUser(id);
+
+        if (!user) {
+            throw new NotFound(`User with id ${id} not found!`);
+        }
+
+        if (!user.delReq) {
+            throw new RootsExeption(
+                `User with id ${id} does not send delete request.`
+            );
+        }
+
+        await userRep.deleteUser(id);
+    }
+
+    async getUser(id){
+        const user=userRep.getUser(id);
+
+        return user;
+    }
+
+    async updateUser(id,field){
+        await userRep.updateUser(id,field);
+    }
+
+    async addUserRole(id, value) {
+        const user = await this.getUser(id);
+
+        if (!user) {
+            throw new NotFound(`User with id ${id} not found!`);
+        }
+
+        const role = await roleService.getRoleByValue(value);
+
+        if (!role) {
+            throw new NotFound(`Role '${role}' not found!`);
+        }
+
+        await user.addRole(role, {
+            through: { userId: user.id, roleId: role.id }
+        });
+    }
+
+    async getUserRole(id) {
+        const user = await this.getUser(id);
+
+        if (!user) {
+            throw new NotFound(`User with id ${id} not found!`);
+        }
+
+        const roles = await roleService.getUserRole(id);
+
+        return roles;
+    }
+    async delReq(id) {
+        await userRep.sendReq(id, false);
+    }
+
+    async sendReq(id) {
+        await userRep.sendReq(id, true);
+    }
+    async deleteAllUsers() {
+        await userRep.deleteAllUsers();
     }
 }
 
-module.exports= new UserServices();
+module.exports= new User();
