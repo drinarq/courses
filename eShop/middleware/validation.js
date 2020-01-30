75e0d493afee4828a44ac35595fe17246de1735a
drinar <drinarq@gmail.com>
2020-01-30 23:43:52 +0300

    fix valudation

--- eShop/middleware/validation.js
+++ eShop/middleware/validation.js
@@ -5,7 +5,7 @@ const ValidationError = require('../errors/ValidationError.js');
 module.exports = (shema) => {
     return (req, res, next) => {
         const isNotValid = Joi.validate(req.body, shema).error;
-
+        console.log(isNotValid);
         if (isNotValid) {
             next(new ValidationError(isNotValid.message));
         }
