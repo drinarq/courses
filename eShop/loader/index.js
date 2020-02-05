afed8392a2fb40d8469af742c679145cb4949b72
drinar <drinarq@gmail.com>
2020-02-05 21:02:03 +0300

    add cron job start

--- eShop/loader/index.js
+++ eShop/loader/index.js
@@ -3,10 +3,11 @@ const express = require('express');
 const expressLoader = require('./express.js');
 const passportLoader = require('./passport.js');
 const routesLoader = require('./routes.js');
+const cronJob=require('../cron/deleteUsers.js');
 
 const router = express.Router();
 router.use(expressLoader);
 router.use(passportLoader);
 router.use(routesLoader);
-
+cronJob.start();
 module.exports = router;
