40f09c2a37ebaa00ddb69ef57badafb0e25e6190
drinar <drinarq@gmail.com>
2020-02-05 20:38:49 +0300

    delete routes and add use loader

--- eShop/index.js
+++ eShop/index.js
@@ -2,24 +2,10 @@ const express = require("express");
 const app = express();
 const pasport=require('passport');
 const session=require('express-session');
-const GoodsRouter = require('./routes/goods.js');
-const UserRouter = require('./routes/user.js');
-const LoginRouter=require('./routes/authorization.js');
-const RoleRouter=require('./routes/role.js');
-const passportLocal=require('./passport.js');
-const sessionConfig=require('./configuration/session.json');
+const loader=require('./loader/index.js');
 
-app.use(session(sessionConfig));
-passportLocal(pasport);
-app.use(pasport.initialize());
-app.use(pasport.session());
-app.use(express.urlencoded({extended:false}));
-app.use(express.json());
-app.use("/Goods",GoodsRouter);
-app.use("/User",UserRouter);
-app.use("/authenticate",LoginRouter);
-app.use("/roles",RoleRouter);
-app.use(function (req, res, next) {
+app.use(loader);
+app.use((req, res, next)=> {
     res.status(404).send("Not Found")
 });
 
