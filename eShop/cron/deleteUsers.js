27cbffdf18c40d47304f565401b2f354355a0c6b
drinar <drinarq@gmail.com>
2020-02-05 21:05:40 +0300

    micro fix

--- eShop/cron/deleteUsers.js
+++ eShop/cron/deleteUsers.js
@@ -2,7 +2,7 @@ const CronJob=require('cron').CronJob;
 const userRepository=require('../repository/user.js');
 const job=new CronJob('0 0 * * * *',async ()=> {
 
-    const users=await userRepository.getReqUsers;
+    const users=await userRepository.getReqUsers();
     for (let value of users){
         await userRepository.deleteUser(value.id);
     }
