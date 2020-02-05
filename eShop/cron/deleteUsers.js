c5d2841e3c32bb24e3d1ed25895a3d38b464fc5c
drinar <drinarq@gmail.com>
2020-02-05 21:01:04 +0300

    add cron job for auto deleting users

---
+++ eShop/cron/deleteUsers.js
@@ -0,0 +1,12 @@
+const CronJob=require('cron').CronJob;
+const userRepository=require('../repository/user.js');
+const job=new CronJob('0 0 * * * *',async ()=> {
+
+    const users=await userRepository.getReqUsers;
+    for (let value of users){
+        await userRepository.deleteUser(value.id);
+    }
+});
+
+
+module.exports=job;
