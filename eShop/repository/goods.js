1c406fa76822df92b2fa9e5c443d0c5094dea9c9
drinar <drinarq@gmail.com>
2020-02-11 18:19:22 +0300

    add paginate

--- eShop/repository/goods.js
+++ eShop/repository/goods.js
@@ -1,35 +1,39 @@
 const goodsModule=require("../models/goods.js");
 const markRep=require("./mark.js");
 const op=require('sequelize').Op;
-const fs=require('fs');
+const paginate=require('../helpers/paginate.js');
 const isEmpty=require('../helpers/isEmpty.js');
+
 class Goods {
 
     async addProduct(product){
+
         await goodsModule.create(product);
     }
 
     async getGoods(sort){
+
         let result=[];
         let zero=[];
         let goods=[];
 
-        if(isEmpty(sort)) {
-            goods = await goodsModule.findAll();
+        let page=isEmpty(sort.page)?1:parseInt(sort.page);
+        let pageSize=isEmpty(sort.pageSize)?10:parseInt(sort.pageSize);
+
+        if(isEmpty(sort.sortBy)) {
+            goods = await goodsModule.findAll(paginate({where:{},
+            },{page,pageSize},));
         }
 
         else if(sort.sortBy==='image'){
-            goods=await goodsModule.findAll({where:{image: {[op.ne]: null},amount:{[op.gt]:0}}});
-            zero=await goodsModule.findAll({where: {image: {[op.ne]: null},amount:0}});
-            console.log(zero);
 
+            goods=await goodsModule.findAll(paginate({where:{image: {[op.ne]: null},amount:{[op.gt]:0}}},{page,pageSize},));
+            zero=await goodsModule.findAll(paginate({where: {image: {[op.ne]: null},amount:0}},{page,pageSize}));
         }
         else{
-            goods=await goodsModule.findAll({where:{amount:{[op.gt]:0}},order:[[sort.sortBy,'ASC']]});
 
-            zero=await goodsModule.findAll({where:{amount:0},order:[[sort.sortBy,'ASC']]});
-
-
+            goods=await goodsModule.findAll(paginate({where:{amount:{[op.gt]:0}},order:[[sort.sortBy,'ASC']]},{page,pageSize}));
+            zero=await goodsModule.findAll(paginate({where:{amount:0},order:[[sort.sortBy,'ASC']]},{page,pageSize}));
         }
 
         result=goods.concat(zero);
@@ -49,7 +53,9 @@ class Goods {
     }
 
     async getProduct(id){
+        
         const product = await goodsModule.findAll({where:{id:id}});
+
         for(let value of product){
             let productId=value.id;
             value.dataValues["Marks amount"]=await markRep.markCount(productId);
@@ -61,12 +67,12 @@ class Goods {
     }
 
     async updateProduct(id,field){
+
         const product=await goodsModule.findOne({where:{id:id}});
         const date={update_date:new Date()};
         await product.update(field);
         await product.update(date);
     }
-
 
 
     async setMark(userId,value,productId){
