5cc6f27720c561f0f09c4d7ca0b19a23709a87b6
drinar <drinarq@gmail.com>
2020-02-11 18:20:37 +0300

    add paginate

--- eShop/services/goods.js
+++ eShop/services/goods.js
@@ -3,50 +3,38 @@ const GoodsRep=require("../repository/goods.js");
 class Goods{
 
     async addProduct(product){
+
         await GoodsRep.addProduct(product);
     }
 
-    async getGoods(){
-        const goods=await GoodsRep.getGoods();
+    async getGoods(sort){
+
+        const goods=await GoodsRep.getGoods(sort);
 
         return goods;
     }
 
     async deleteProduct(id){
+
         await GoodsRep.deletePoduct(id);
     }
 
     async getProduct(id){
+
         const product=await GoodsRep.getProduct(id);
 
         return product;
     }
 
     async updateProduct(id,field){
+
         await GoodsRep.updateProduct(id,field);
     }
 
-    async getGoodsByName(){
-        const goods=await GoodsRep.getGoodsByName();
 
-        return goods;
-    }
-
-
-
-    async getGoodsByUpDate(){
-        const goods=await GoodsRep.getGoodsByUpDate();
-        console.log("adasdasd");
-        return goods;
-    }
-
-    async getGoodsWithImage(){
-        const goods=await GoodsRep.getGoodsWithImage();
-
-        return goods;
-    }
 
     async setMark(userId,value,productId){
+
         const mark=await GoodsRep.setMark(userId,value,productId);
 
         return mark;
@@ -54,6 +42,7 @@ class Goods{
     }
 
     async deleteMark(userId,productId){
+
         return await GoodsRep.deleteMark(userId,productId);
     }
 }
--- eShop/services/user.js
+++ eShop/services/user.js
@@ -1,5 +1,5 @@
 const userRep=require('../repository/user.js');
-const constRole = require('../helpers/constRole.js');
+const constRole = require('../constants/constRole.js');
 const roleService = require('./role.js');
 const RootsExeption = require('../errors/RootsExeption.js');
 const NotFound = require('../errors/NotFound.js');
@@ -7,6 +7,7 @@ const AlreadyExist=require('../errors/AlredyExist.js');
 const userRoleRep=require('../repository/roles.js');
 class User{
     async getUserByEmail(email) {
+
         const user = await usersRepository.getUserByEmail(email);
 
         return user;
@@ -14,9 +15,11 @@ class User{
     async addUser(value){
 
         const UserExist=!!(await userRep.getUserByEmail(value.email));
+
         if(UserExist){
             throw new AlreadyExist(`user with ${value.email} already exist`);
         }
+
         const user= await userRep.addUser(value);
 
         const role=await roleService.getRoleByValue(constRole.user);
@@ -30,8 +33,9 @@ class User{
 
     }
 
-    async getUsers(){
-        const users=await userRep.getUsers();
+    async getUsers(query){
+
+        const users=await userRep.getUsers(query);
 
         return users;
     }
@@ -45,26 +49,31 @@ class User{
         }
 
         if (!user.delReq) {
+
             throw new RootsExeption(
                 `User with id ${id} does not send delete request.`
             );
         }
+
         await roleService.delUserRole(id);
         await userRep.deleteUser(id);
     }
 
     async getUser(id){
+
         const user=userRep.getUser(id);
 
         return user;
     }
 
     async updateUser(id,field){
+
         await userRep.updateUser(id,field);
     }
 
 
     async getUserRole(id) {
+
         const user = await this.getUser(id);
 
         if (!user) {
@@ -76,14 +85,17 @@ class User{
         return roles;
     }
     async delReq(id) {
+
         await userRep.sendReq(id, false);
     }
 
     async sendReq(id) {
+
         await userRep.sendReq(id, true);
     }
 
     async updateUserRole(userId,roleId){
+
         await roleService.updateUserRole(userId,roleId);
     }
 
