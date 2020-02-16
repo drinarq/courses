d613a286d3b96bcbac64760d21c35407530a10b0
drinar <drinarq@gmail.com>
2020-02-16 16:55:53 +0300

    add dependecies tag and goods

--- eShop/models/dependencies.js
+++ eShop/models/dependencies.js
@@ -3,24 +3,39 @@ const Roles = require('./role.js');
 const Users = require('./user.js');
 const UsersRoles = require('./usersRole.js');
 const Marks = require('./mark.js');
+const Tags=require('./tags.js');
+const GoodsTags=require('./usersRole.js');
 
 Users.belongsToMany(Roles, {
     through: UsersRoles,
     foreignKey: 'user_id',
-    onDelete: 'SET NULL'
+    onDelete: 'SET NULL',
 });
+
 Roles.belongsToMany(Users, {
     through: UsersRoles,
     foreignKey: 'role_id',
-    onDelete: 'SET NULL'
+    onDelete: 'SET NULL',
 });
 
 Users.hasMany(Marks, {
     foreignKey: 'user_id',
-    onDelete: 'CASCADE'
+    onDelete: 'CASCADE',
 });
 
 Goods.hasMany(Marks, {
     foreignKey: 'product_id',
-    onDelete: 'CASCADE'
+    onDelete: 'CASCADE',
 });
+
+Goods.belongsToMany(Tags,{
+    through:GoodsTags,
+    foreignKey:'product_id',
+    onDelete:'SET NULL',
+})
+
+Tags.belongsToMany(Goods,{
+    through:GoodsTags,
+    foreignKey:'tag_id',
+    onDelete:'SET NULL',
+})
