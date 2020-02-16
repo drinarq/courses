ca8d66bd74fdc0c43c16c1a3f26e3e7ab163cd5e
drinar <drinarq@gmail.com>
2020-02-16 16:51:21 +0300

    add getting goods by tags and upload image on server

--- eShop/services/goods.js
+++ eShop/services/goods.js
@@ -1,8 +1,19 @@
 const GoodsRep=require("../repository/goods.js");
+const TagService=require('./tag.js');
+const path=require('path');
+const fs=require('fs');
+const env=require('../configuration/environment.js');
+const NotFoundException=require('../errors/NotFound.js');
 
-class Goods{
+class GoodsService{
 
     async addProduct(product){
+
+        const imageName=path.basename(product.image);
+        const image=fs.readFileSync(product.image);
+
+        fs.writeFileSync(env.goods.uploadsPath+imageName,image);
+
 
         await GoodsRep.addProduct(product);
     }
@@ -16,7 +27,7 @@ class Goods{
 
     async deleteProduct(id){
 
-        await GoodsRep.deletePoduct(id);
+        await GoodsRep.deleteProduct();
     }
 
     async getProduct(id){
@@ -45,6 +56,65 @@ class Goods{
 
         return await GoodsRep.deleteMark(userId,productId);
     }
+
+    async addProductTag(value,productId){
+        const product=await this.getProduct(productId);
+
+        const tag=await TagService.getByValue(value);
+
+        if(!product){
+            throw new NotFoundException('product not found.');
+
+        }
+
+        if(!tag){
+            throw new NotFoundException('tag not found.');
+
+        }
+
+        return TagService.addProductTag(productId,tag.dataValues.id);
+    }
+
+    async delProductTag(productId){
+        const product=await this.getProduct(productId);
+
+        if(!product){
+            throw new NotFoundException('product not found.');
+
+        }
+
+        return await TagService.delProductTag(productId);
+
+    }
+
+    async getProductTags(id){
+        const product=await this.getProduct(id);
+
+        if(!product){
+            throw new NotFoundException('product not found.');
+
+        }
+
+        const tags= await TagService.getProductTags(id);
+
+        if(!tags){
+            throw new NotFoundException('product tags not found');
+        }
+        return tags;
+
+    }
+
+    async getGoodsByTag(value){
+        const tag=await TagService.getByValue(value);
+
+        if(!tag){
+            throw new NotFoundException('tag not found.');
+
+        }
+
+        return await TagService.getGoodsByTag(value);
+
+    }
 }
 
-module.exports= new Goods();
+module.exports= new GoodsService();
