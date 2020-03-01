60a3f64977e5b2cd64a2eb886a6ef369d95097ed
drinar <drinarq@gmail.com>
2020-03-01 18:12:44 +0300

    add logger middleware

--- eShop/middleware/logger.js
+++ eShop/middleware/logger.js
@@ -1,14 +1,32 @@
 const logger=require('../logger/mongoLogger.js');
 const userService=require('../services/user.js');
 
-class LoggerHelper {
+class Logger {
 
-    async addLog(data,id){
+    async addErrorLog(data,id){
         const role=await userService.getUserRole(id);
         data.userRole=role.value;
         await logger.addErrorLog(data);
     }
 
+    async addEventLog(req,res,next){
+        let user={};
+        if(req.body.email!==undefined){
+            user=await userService.getUserByEmail(req.body.email);
+        }
+        const data={
+            userId:user.id||req.user.id,
+            method:req.method,
+            path:req._parsedOriginalUrl.path,
+            date:new Date()
+        };
+
+        await logger.addEventLog(data);
+
+        next();
+
+    }
+
 }
 
-module.exports=new LoggerHelper();
+module.exports=new Logger();
